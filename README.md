19673924be0220e5294afaa4051cc3469a1a1ffa
Takayama Fumihiko <tekezo@pqrs.org>
2018-12-30 17:12:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+[![Build Status](https://travis-ci.org/pqrs-org/cpp-osx-session.svg?branch=master)](https://travis-ci.org/pqrs-org/cpp-osx-session)
+[![License](https://img.shields.io/badge/license-Boost%20Software%20License-blue.svg)](https://github.com/pqrs-org/cpp-osx-session/blob/master/LICENSE.md)
+
+# cpp-osx-session
+
+A utility of `SecStaticCodeRef`.
+
+## Requirements
+
+cpp-osx-session depends the following classes.
+
+- [pqrs::cf::array](https://github.com/pqrs-org/cpp-cf-array).
+- [pqrs::cf::dictionary](https://github.com/pqrs-org/cpp-cf-dictionary).
+- [pqrs::cf::number](https://github.com/pqrs-org/cpp-cf-number).
+- [pqrs::cf::string](https://github.com/pqrs-org/cpp-cf-string).
+
+## Install
+
+### Using package manager
+
+You can install `include/pqrs` by using [cget](https://github.com/pfultz2/cget).
+
+```shell
+cget install pqrs-org/cget-recipes
+cget install pqrs-org/cpp-osx-session
+```
+
+### Manual install
+
+Copy `include/pqrs` directory into your include directory.
---
+++ example/main.cpp
@@ -0,0 +1,43 @@
+#include <csignal>
+#include <iostream>
+#include <pqrs/osx/session.hpp>
+
+namespace {
+auto global_wait = pqrs::make_thread_wait();
+}
+
+int main(void) {
+  std::signal(SIGINT, [](int) {
+    global_wait->notify();
+  });
+
+  auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+  auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+
+  auto monitor = std::make_shared<pqrs::osx::session::monitor>(dispatcher);
+
+  monitor->console_user_id_changed.connect([](auto&& console_user_id) {
+    std::cout << "console_user_id_changed (console_user_id:";
+    if (console_user_id) {
+      std::cout << *console_user_id;
+    } else {
+      std::cout << "empty";
+    }
+    std::cout << ")" << std::endl;
+  });
+
+  monitor->async_start(std::chrono::milliseconds(1000));
+
+  // ============================================================
+
+  global_wait->wait_notice();
+
+  // ============================================================
+
+  monitor = nullptr;
+
+  dispatcher->terminate();
+  dispatcher = nullptr;
+
+  return 0;
+}
